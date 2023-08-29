fe26704fe4675bff6946e06e298d7945382fbf09
Terry Luo <z5308257@ad.unsw.edu.au>
2023-08-29 12:20:10 +1000

    Frontend Application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
 function App() {
+  const [familyMembers, setFamilyMembers] = useState([]);
+  const [selectedMember, setSelectedMember] = useState(null);
+  
+
+  useEffect(() => {
+    // Fetch family members from the backend when the component mounts
+    fetch('http://127.0.0.1:5000/get_family_members')
+      .then(response => response.json())
+      .then(data => {setFamilyMembers(data); return data;}) 
+      .catch(error => console.error('Error fetching family members:', error));
+  }, []);
+
+  const handleMemberClick = (member) => {
+    setSelectedMember(member);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Family Members</h1>
+      <div className="family-buttons">
+        {familyMembers.map(member => (
+          <button
+            key={member.id}
+            onClick={() => handleMemberClick(member)}
+            className="member-button"
+          >
+            {member["First Name"]}
+          </button>
+        ))}
+      </div>
+      {selectedMember && (
+        <div className="member-details">
+          <h2>{selectedMember["First Name"]}</h2>
+          <p>Age: {selectedMember["Age"]}</p>
+          <p>Gender: {selectedMember["Gender"]}</p>
+          <p>Occupation: {selectedMember["Occupation"]}</p>
+          <p>Ethnicity: {selectedMember["Ethnicity"]}</p>
+        </div>
+      )}
     </div>
   );
 }
